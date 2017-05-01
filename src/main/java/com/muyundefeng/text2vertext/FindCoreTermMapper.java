de371698c9b5d6ce432699d3ff7da90cafb297d1
lisheng <2533604335@qq.com>
2017-05-01 22:34:05 +0800

    modify termcut

--- src/main/java/com/muyundefeng/text2vertext/FindCoreTermMapper.java
+++ src/main/java/com/muyundefeng/text2vertext/FindCoreTermMapper.java
@@ -24,11 +24,15 @@ public class FindCoreTermMapper extends Mapper<NullWritable, Cluster, NullWritab
     public static List<String> terms = new ArrayList<String>();
 
     //保存全局cluster
-    public static Map<String, Cluster> clusters = new HashMap<String, Cluster>();
+    public static Map<String, Cluster> clusters = new HashMap<String, Cluster>();//更新聚类
 
     public static double minRmCut = Double.MIN_VALUE;
 
     public static String coreTerm = null;
+
+    public static final double TRESH_HOLD = 0.1;
+
+    public static int cluster_number = 1;
 
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
@@ -47,67 +51,88 @@ public class FindCoreTermMapper extends Mapper<NullWritable, Cluster, NullWritab
         clusters.put(GenerateLabelUtils.getLabel(), value);
     }
 
+    /**
+     * cleanup 函数要循环执行，直到小于相关的阈值
+     *
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
-        Cluster cb = new Cluster();
-        Cluster cb1 = new Cluster();
-        for (Map.Entry entry : clusters.entrySet()) {
-            String label = entry.getKey().toString();//得到该聚类的标签
-            //得到该聚类中的所有关键词
-            Cluster cluster = ((Cluster) (entry.getValue()));//得到该聚类
-            //得到聚类中的所有向量
-            List<Vertext> vertexts = cluster.getVertextes();
-            Set<String> terms = new HashSet<String>();
-            //得到所有向量中的单词词汇
-            for (Vertext vertext : vertexts) {
-                String url = vertext.getUrl();
-                String afterUrl = url.replaceAll(StopWordsDict.PROTOCAL, "").replaceAll(StopWordsDict.DATE, "")
-                        .replaceAll(StopWordsDict.SUFFIX, "").replaceAll(StopWordsDict.TRIPLE_W, "");
-                String words[] = afterUrl.split(StopWordsDict.SEPARATOR);
-                for (String word : words) {
-                    terms.add(word);
-                }
-            }
-            //分析该聚类中的所有单词
-            for (String term : terms) {
-                List<Vertext> list = new ArrayList<Vertext>();
-                List<Vertext> anotherList = new ArrayList<Vertext>();
+
+        while (true) {
+            //记录原始的rmCut标准
+            double originalRmCut = calRMCut(clusters);
+            for (Map.Entry entry : clusters.entrySet()) {
+                String label = entry.getKey().toString();//原始的聚类标签
+                //得到该聚类中的所有关键词
+                Cluster cluster = ((Cluster) (entry.getValue()));//得到该聚类
+                //得到聚类中的所有向量
+                List<Vertext> vertexts = cluster.getVertextes();
+                Set<String> terms = new HashSet<String>();
+                //得到所有向量中的单词词汇
                 for (Vertext vertext : vertexts) {
-                    if (vertext.getUrl().contains(term)) {
-                        list.add(vertext);
-                    } else {
-                        anotherList.add(vertext);
+                    String url = vertext.getUrl();
+                    String afterUrl = url.replaceAll(StopWordsDict.PROTOCAL, "").replaceAll(StopWordsDict.DATE, "")
+                            .replaceAll(StopWordsDict.SUFFIX, "").replaceAll(StopWordsDict.TRIPLE_W, "");
+                    String words[] = afterUrl.split(StopWordsDict.SEPARATOR);
+                    for (String word : words) {
+                        terms.add(word);
                     }
                 }
-                //根据单词是否存在的情况之下，一个聚类产生两个新的聚类，构建新的两个聚类
-                clusters.remove(cluster);//移除去掉原来的聚类
-                //新的聚类一分为二，产生一个新的聚类集合
-                Cluster clusterk1 = new Cluster();
-                clusterk1.setVertext(list);
-                clusterk1.setCoreTerm("null");
-                Cluster clusterk2 = new Cluster();
-                clusterk2.setVertext(anotherList);
-                clusterk2.setCoreTerm("null");
-                clusters.put(GenerateLabelUtils.getLabel(), clusterk1);//为新的聚类打标签
-                clusters.put(GenerateLabelUtils.getLabel(), clusterk2);
+                //分析该聚类中的所有单词
+                for (String term : terms) {
+                    List<Vertext> list = new ArrayList<Vertext>();
+                    List<Vertext> anotherList = new ArrayList<Vertext>();
+                    for (Vertext vertext : vertexts) {
+                        if (vertext.getUrl().contains(term)) {
+                            list.add(vertext);
+                        } else {
+                            anotherList.add(vertext);
+                        }
+                    }
 
-                if (calRMCut(clusters) < minRmCut) {
-                    coreTerm = term;
-                    minRmCut = calRMCut(clusters);
-                    cb.setVertext(list);
-                    cb.setCoreTerm(coreTerm);
-                    cb1.setCoreTerm(coreTerm + "-except");
-                    cb1.setVertext(anotherList);
+                    //根据单词是否存在的情况之下，一个聚类产生两个新的聚类，构建新的两个聚类
+                    Cluster originaCluster = clusters.remove(cluster);//移除去掉原来的聚类
+                    //新的聚类一分为二，产生一个新的聚类集合
+                    Cluster clusterk1 = new Cluster();
+                    clusterk1.setVertext(list);
+                    clusterk1.setCoreTerm(term);//设定核心词汇
+                    Cluster clusterk2 = new Cluster();
+                    clusterk2.setVertext(anotherList);
+                    clusterk2.setCoreTerm("not " + term);//非该聚类，不设定核心词汇，前缀用not来代替
+                    String label1 = GenerateLabelUtils.getLabel();
+                    String label2 = GenerateLabelUtils.getLabel();
+                    clusters.put(label1, clusterk1);//为新的聚类打标签
+                    clusters.put(label2, clusterk2);
+
+                    if (calRMCut(clusters) < minRmCut) {
+                        coreTerm = term;
+                        minRmCut = calRMCut(clusters);
+                        cluster_number++;
+                    } else {
+                        //如果不满足上述条件，还原clusters
+                        clusters.remove(label1);
+                        clusters.remove(label2);
+                        clusters.put(label, originaCluster);
+                    }
                 }
             }
+            double newRmCut = calRMCut(clusters);
+            if (originalRmCut - newRmCut < TRESH_HOLD) {
+                break;
+            }
         }
-        //写入文件
-        context.write(null, cb);
-        context.write(null, cb1);
+        System.out.println("cluster is:"+clusters);
+        System.out.println("cluster number ="+cluster_number);
+        //写入文件，非写入文件
+//        context.write(null, cb);
+//        context.write(null, cb1);
     }
 
     public double calRMCut(Map<String, Cluster> clusters) {
-        List<Cluster> clusterList = new ArrayList<Cluster>();
+        List<Cluster> clusterList = new ArrayList<Cluster>();//临时变量，复制，不影响原来的cluster
         for (Map.Entry entry : clusters.entrySet()) {
             Cluster cluster = (Cluster) entry.getValue();
             clusterList.add(cluster);
@@ -120,7 +145,7 @@ public class FindCoreTermMapper extends Mapper<NullWritable, Cluster, NullWritab
             //c-ck
             tmp.remove(ck);
             List<Cluster> c_ck = tmp;
-            tmp = clusterList;//还原tmp链表
+            tmp.add(ck);//还原tmp链表
             sum += calCulateMolecule(ck, c_ck) / calCulateDenominator(ck);
         }
         return sum;
